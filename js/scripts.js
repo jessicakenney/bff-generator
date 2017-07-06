f1ac268501e93a6ca8e8e13ecb8996be4dd88d7e
Jessica Sheridan and Shyamal Punekar <shyamal@>
2017-07-06 15:57:29 -0700

    Additional input forms and variable assignments for BFF Generator.

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#bff-generator").submit(function(event) {
+
+  var pet = $("#pet").val();
+  var age = parseInt($("#age").val());
+  var male = $("input#male").val();
+  var female = $("input#female").val();
+
+  if(female && pet=="kitty" && age = 1){
+    var bff = ;
+  } else if ( ){
+    
+  }
+
+  event.preventDefault();
+  });
+});
